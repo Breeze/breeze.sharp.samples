67be46299e261c4b136456a26fe732e3899914f3
Jay Traband <jayt@ideablade.com>
2014-04-08 12:12:04 -0700

    removed breeze.js related code from server and simplified client app.config

--- ToDo/Todo-Server/Scripts/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="jquery-1.8.3.js" />
-/// <reference path="knockout-2.2.0.debug.js" />
-/// <reference path="q.js" />
-/// <reference path="breeze.debug.js" />
-/// <reference path="toastr.js" />
-/// <reference path="app/viewModel.js" />
