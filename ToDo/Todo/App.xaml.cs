3166864145c9321e7b215c0054c64bcba9c3d6b4
Jay Traband <jayt@ideablade.com>
2014-04-08 12:02:24 -0700

    cleanup - remove data and ef refs from client app

--- ToDo/Todo/App.xaml.cs
+++ ToDo/Todo/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
-using System.Data;
+
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
