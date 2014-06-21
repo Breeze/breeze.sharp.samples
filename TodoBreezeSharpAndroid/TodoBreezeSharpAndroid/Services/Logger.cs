a61738ada5459b4626148254bd77614b419fe11a
Ward Bell <wardbell@hotmail.com>
2014-06-20 23:16:57 -0700

    Added Android logging See Jesse Liberty post on this: http://blog.falafel.com/Blogs/jesseliberty/jesse-liberty/2013/09/16/xamarin-for-android-logging Coincidentally committing update to Resource.Designer due to Android runtime upgrade

--- TodoBreezeSharpAndroid/TodoBreezeSharpAndroid/Services/Logger.cs
+++ TodoBreezeSharpAndroid/TodoBreezeSharpAndroid/Services/Logger.cs
@@ -1,5 +1,6 @@
 using System;
 using Android.App;
+using Util = Android.Util;
 using Android.Views;
 using Android.Widget;
 
@@ -21,6 +22,7 @@ namespace TodoBreezeSharpAndroid.Services
     public Logger(Activity activity)
     {
       _activity = activity;
+      Util.Log.Info(LOG_TAG, "Logger created");
     }
 
     public void Error(Exception ex)
@@ -32,20 +34,24 @@ namespace TodoBreezeSharpAndroid.Services
     public virtual void Error(string message)
     {
       ShowToast("Error: " + message);
+      Util.Log.Error(LOG_TAG, message);
       Console.WriteLine("Error: " + message);
     }
     public virtual void Info(string message)
     {
       ShowToast("Info: " + message);
+      Util.Log.Info(LOG_TAG, message);
       Console.WriteLine("Info: " + message);
     }
     public void Log(string message)
     {
+      Util.Log.Info(LOG_TAG, message);
       Console.WriteLine("Log: " + message);
     }
     public virtual void Warning(string message)
     {
       ShowToast("Warning: " + message);
+      Util.Log.Warn(LOG_TAG, message);
       Console.WriteLine("Warning: " + message);
     }
 
@@ -55,5 +61,7 @@ namespace TodoBreezeSharpAndroid.Services
       toast.SetGravity(GravityFlags.Top | GravityFlags.Center, 0, 100);
       toast.Show();
     }
+
+    private const string LOG_TAG = "BZ#";
   }
 }
