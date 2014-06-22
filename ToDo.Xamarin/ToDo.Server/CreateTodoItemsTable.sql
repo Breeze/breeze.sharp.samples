43c9c3bbed65fc9a810e5512eb15a7d9c893954a
Ward Bell <wardbell@hotmail.com>
2014-06-22 00:12:25 -0700

    Publish to azure "xamarintasky" website and database (Ward's account)

---
+++ ToDo.Xamarin/ToDo.Server/CreateTodoItemsTable.sql
@@ -0,0 +1,19 @@
+﻿-- =========================================
+-- Create table template SQL Azure Database 
+-- Script Date: 6/21/2014 10:49:58 PM
+-- =========================================
+
+IF OBJECT_ID('dbo.TodoItems', 'U') IS NOT NULL
+  DROP TABLE [dbo].[TodoItems]
+GO
+
+CREATE TABLE [dbo].[TodoItems](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [nvarchar](30) NOT NULL,
+	[Notes] [nvarchar](max) NULL,
+	[CreatedAt] [datetime] NOT NULL,
+	[IsDone] [bit] NOT NULL,
+	[IsArchived] [bit] NOT NULL,
+ CONSTRAINT [PK_dbo.TodoItems] PRIMARY KEY CLUSTERED ([Id] ASC)
+ )
+GO
