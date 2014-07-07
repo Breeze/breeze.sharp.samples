d2908a6baf63363611c97d1fc26c276fbefb7f3d
William Jensen <williamj@ideablade.com>
2014-07-07 15:12:37 -0700

    Added additional examples of server-side interception.

--- DocCode/Client/DocCode.Client/InterceptionTests.cs
+++ DocCode/Client/DocCode.Client/InterceptionTests.cs
@@ -36,38 +36,79 @@ namespace Test_NetClient
         }
 
         /// <summary>
-        /// This test demonstrates interception of entity save requests on the server side.
-        /// On the server, the TodosRepository attaches a method to the data context as the BeforeEntitySaveDelegate:
-        /// <code>
-        ///     public TodosRepository() {
-        ///         _contextProvider.BeforeSaveEntityDelegate = BeforeSaveEntity;
-        ///     }
-        /// </code>
-        /// The delegate function modifies the description of the entity being saved before saving it
-        /// and returning it to the client:
-        /// <code>
-        ///     private bool BeforeSaveEntity(EntityInfo arg) {
-        ///         var todo = arg.Entity as TodoItem;
-        ///         if (todo != null && todo.Description.Contains("INTERCEPT")) {
-        ///             todo.Description += " SAVED!";
-        ///         }
-        ///         return true;
-        ///     }
-        /// </code>
-        /// The modification is automatically merged back into the entity on the client side.
+        /// These tests demonstrate interception of entity save requests on the server side.
+        /// On the server, the TodosRepository attaches methods to the data context that modify
+        /// the Description fieldsof the entities being saved before and after the save operation.
+        /// 
+        /// The modified Description field is automatically merged back into the entity on the client side,
+        /// allowing it to be tested.
+        /// 
+        /// See TodosRepository.cs in the DocCode.DataAccess.EF project server-side project.
+        /// 
         /// </summary>
         /// <returns></returns>
         [TestMethod]
-        public async Task BeforeSaveEntityTest() {
+        public async Task InterceptAndSaveTest() {
 
             var entityManager = await Test_NetClient.TestFns.NewEm(_todosServiceName);
+
             var todo = entityManager.CreateEntity<TodoItem>();
             todo.Initialize();
-            todo.Description = "INTERCEPT";
+            todo.Description = "INTERCEPT SAVE";
+
             var saveResult = await entityManager.SaveChanges();
-            var description = todo.Description;
-            Assert.IsTrue(description.Contains("SAVED!"), "Saved Todo should contain the string 'SAVED!'");
+            Assert.IsTrue(todo.Description.Contains("SAVED"), "Saved Todo should contain the string 'SAVED'");
+
+            // The BeforeSaveEntities delegate adds the number of items to be saved to the Description field
+            Assert.IsTrue(todo.Description.Contains("(1)"));
+
+            // The AfterSaveEntities delegate add the number of saved items and key mappings to the description field
+            Assert.IsTrue(todo.Description.Contains("(1, 1)"));
         }
+
+        /// <summary>
+        /// Test the ability of the interceptor to exclude an entity from the save
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task InterceptAndExcludeTest() {
+
+            var entityManager = await Test_NetClient.TestFns.NewEm(_todosServiceName);
+
+            var todo = entityManager.CreateEntity<TodoItem>();
+            todo.Initialize();
+            todo.Description = "INTERCEPT EXCLUDE";
+
+            var saveResult = await entityManager.SaveChanges();
+
+            var description = todo.Description;
+            Assert.IsFalse(todo.Description.Contains("SAVED"), "Excluded description should not contain 'SAVED'");
+            Assert.IsTrue(todo.Description.Contains("INTERCEPT EXCLUDE"), "Excluded description should be unmodified");
+            Assert.IsTrue(todo.Id < 0, "Excluded entity should still have temporary key value");
+        }
+
+        /// <summary>
+        /// Test the ability of the interceptor to abort the save operation 
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task InterceptAndFailTest() {
+
+            var entityManager = await Test_NetClient.TestFns.NewEm(_todosServiceName);
+
+            var todo = entityManager.CreateEntity<TodoItem>();
+            todo.Initialize();
+            todo.Description = "INTERCEPT FAIL";
+
+            try {
+                var saveResult = await entityManager.SaveChanges();
+                Assert.Fail("Saving of Todo with INTERCEPT FAIL in description should throw an exception");
+            }
+            catch (Breeze.Sharp.SaveException e) {
+                Assert.IsTrue(e.Message.Contains("INTERCEPT FAIL"));
+            }
+        }
+
     }
 }
 
--- DocCode/Server/DocCode.DataAccess.EF/TodosRepository.cs
+++ DocCode/Server/DocCode.DataAccess.EF/TodosRepository.cs
@@ -20,6 +20,8 @@ namespace DocCode.DataAccess
 
         public TodosRepository() {
             _contextProvider.BeforeSaveEntityDelegate = BeforeSaveEntity;
+            _contextProvider.BeforeSaveEntitiesDelegate = BeforeSaveEntities;
+            _contextProvider.AfterSaveEntitiesDelegate = AfterSaveEntities;
         }
 
         public string Metadata
@@ -57,7 +59,7 @@ namespace DocCode.DataAccess
         #region Save Interception
 
         /// <summary>
-        /// Demonstration interceptor
+        /// Demonstration interceptor - called once for each entity to be saved
         /// </summary>
         /// <returns>
         /// True if can save this entity else throw exception
@@ -66,9 +68,61 @@ namespace DocCode.DataAccess
         private bool BeforeSaveEntity(EntityInfo arg) {
             var todo = arg.Entity as TodoItem;
             if (todo != null && todo.Description.Contains("INTERCEPT")) {
-                todo.Description += " SAVED!";
+                if (todo.Description.Contains("SAVE")) {
+                    todo.Description = "INTERCEPT SAVED";
+                    return true;
+                }
+                if (todo.Description.Contains("EXCLUDE")) {
+                    todo.Description = "INTERCEPT EXCLUDED";
+                    return false;
+                }
+                if (todo.Description.Contains("FAIL")) {
+                    throw new InvalidOperationException("Failed due to description contains INTERCEPT FAIL");
+                }
             }
             return true;
+        }
+
+        /// <summary>
+        /// Demonstration pre-save interceptor called once per save request
+        /// </summary>
+        /// <param name="entitiesToBeSaved">
+        /// Dictionary (indexed by entity type) of entities to be saved
+        /// </param>
+        /// <returns>
+        /// A (possibly modified) dictionary of entities to be saved
+        /// </returns>
+        private Dictionary<Type, List<EntityInfo>> BeforeSaveEntities(Dictionary<Type, List<EntityInfo>> entitiesToBeSaved) {
+            var entityInfos = entitiesToBeSaved[typeof(TodoItem)];
+            entityInfos.ForEach(ei =>
+            {
+                var todo = ei.Entity as TodoItem;
+                if (todo.Description.Contains("INTERCEPT")) {
+                    todo.Description += " (" + entityInfos.Count + ")";
+                }
+            });
+
+            return entitiesToBeSaved;
+        }
+
+        /// <summary>
+        /// Demonstration post-save interceptor - called once per save request
+        /// </summary>
+        /// <param name="savedEntities">
+        /// Dictionary (indexed by entity type) of entities to be saved
+        /// </param>
+        /// <param name="keyMappings">
+        /// List of mappings from temporary to store-generated permanent keys
+        /// </param>
+        private void AfterSaveEntities(Dictionary<Type, List<EntityInfo>> savedEntities, List<KeyMapping> keyMappings) {
+            var entityInfos = savedEntities[typeof(TodoItem)];
+            entityInfos.ForEach(ei =>
+            {
+                var todo = ei.Entity as TodoItem;
+                if (todo.Description.Contains("INTERCEPT")) {
+                    todo.Description += " (" + entityInfos.Count + ", " + keyMappings.Count + ")";
+                }
+            });
         }
 
         #endregion SaveInterception
