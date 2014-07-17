d6e59763c2a8a660ff13bc2e89123b2d52b9c810
William Jensen <williamj@ideablade.com>
2014-07-17 15:11:42 -0700

    Added initial version of ConcurrencyTests

---
+++ DocCode/Client/DocCode.Client/ConcurrencyTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Threading;
+
+using Breeze.Sharp.Core;
+using Breeze.Sharp;
+
+using Northwind.Models;
+
+namespace Test_NetClient
+{
+    [TestClass]
+    public class ConcurrencyTests
+    {
+        private String _northwindServiceName;
+        private readonly Guid _alfredsID = Guid.Parse("785efa04-cbf2-4dd7-a7de-083ee17b6ad2");
+
+        [TestInitialize]
+        public void TestInitializeMethod() {
+            _northwindServiceName = "http://localhost:56337/breeze/Northwind/";
+        }
+
+        [TestCleanup]
+        public void TearDown() {
+        }
+
+        [TestMethod]
+        public async Task SimpleConcurrencyFault() {
+
+            Configuration.Instance.ProbeAssemblies(typeof(Customer).Assembly);
+
+            // Query Alfred
+            var entityManager1 = new EntityManager(_northwindServiceName);
+            var query = new EntityQuery<Customer>().Where(c => c.CustomerID == _alfredsID);
+            var alfred1 = (await entityManager1.ExecuteQuery(query)).FirstOrDefault();
+
+            // Query a second copy of Alfred in a second entity manager
+            var entityManager2 = new EntityManager(_northwindServiceName);
+            var alfred2 = (await entityManager2.ExecuteQuery(query)).FirstOrDefault();
+
+            // Modify and save the first Alfred
+            alfred1.ContactTitle += "X";
+            var saveResult1 = await entityManager1.SaveChanges();
+
+            // Attempt to modify and save the second Alfred
+            alfred2.ContactName += "Y";
+            try {
+                var saveResult2 = await entityManager2.SaveChanges();
+            }
+            catch (SaveException e) {
+                var message = e.Message;
+            }
+        }
+    }
+}
+
+
